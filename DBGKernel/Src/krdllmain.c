3a4f5fd04c6ce54a3859a7250d9001320f59a448
wshbnmxy <wshbnmxy@hotmail.com>
2017-07-01 10:50:37 +0800

    Add DllMain func and Memory leak dect

---
+++ DBGKernel/Src/krdllmain.c
@@ -0,0 +1,27 @@
+#ifdef _DEBUG
+#define CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif // _DEBUG
+
+#include <krcommon.h>
+#include <krdata.h>
+
+#include <Windows.h>
+
+BOOL WINAPI DllMain(HANDLE hModule, DWORD dwReason, LPVOID lpReserved) {
+        switch (dwReason)
+        {
+        case DLL_PROCESS_ATTACH:
+                break;
+        case DLL_PROCESS_DETACH:
+                kr_uninitDebugInfo();
+#ifdef _DEBUG
+                _CrtDumpMemoryLeaks();
+#endif // _DEBUG
+                break;
+        default:
+                break;
+        }
+        return TRUE;
+}
